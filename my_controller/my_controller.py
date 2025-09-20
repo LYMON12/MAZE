2e9f5fdfcb4a898d5e9dede5504a1d43d81a01ce
LYMON12 <87726419+LYMON12@users.noreply.github.com>
2025-09-20 20:46:50 -0300

    Atualizar o my_controller.ino

--- my_controller/my_controller.py
+++ my_controller/my_controller.py
@@ -1,32 +1,178 @@
-"""my_controller controller."""
+#include <AFMotor.h>
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+AF_DCMotor esq_tras(1);
+AF_DCMotor dir_tras(2);
+AF_DCMotor dir_frente(3);
+AF_DCMotor esq_frente(4);
 
-# create the Robot instance.
-robot = Robot()
+int dif_motores = 0;
+int vel = 70;
+int distancia_ultra;
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+unsigned long memoria = 0;
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+int pino_esq_fora = 14;
+int pino_esq_dentro = 15;
+int pino_dir_dentro = 16;
+int pino_dir_fora = 17;
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+#define G 32
+#define R 30
+#define B 28
+#define TRIG 50
+#define ECHO 52
 
-    # Process sensor data here.
+int PRETO = 1;
+int BRANCO = 0;
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+unsigned long tempo2 = 0;
+int intervalo = 1000;
 
-# Enter here exit cleanup code.
+// Variáveis dos sensores (inicializadas depois no atualizar_sensores)
+int sensor_esq_dentro;
+int sensor_dir_dentro;
+int sensor_esq_fora;
+int sensor_dir_fora;
+
+// Estados lógicos
+bool todosPreto;
+bool algumPreto;
+bool dentroPreto;
+bool algumEsq;
+bool algumDir;
+bool todosEsq;
+bool todosDir;
+bool foraPreto;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+
+  pinMode(pino_esq_fora, INPUT);
+  pinMode(pino_esq_dentro, INPUT);
+  pinMode(pino_dir_fora, INPUT);
+  pinMode(pino_dir_dentro, INPUT);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  delay(1000);
+}
+
+void loop() {
+  unsigned long agora = millis();
+  if (agora - memoria >= 20) {
+    memoria = agora;
+    atualizar_sensores();
+  }
+
+  frente();
+
+  if (distancia_ultra < 15) {
+    parar();
+    delay(100);
+    desviar();
+  }
+
+  if (dentroPreto == 1) {
+    tras();
+    delay(20);
+    parar();
+    atualizar_sensores();
+    segue_faixa();
+  }
+}
+
+void atualizar_sensores() {
+  distancia_ultra = calc_ultra();
+
+  sensor_esq_dentro = digitalRead(pino_esq_dentro);
+  sensor_dir_dentro = digitalRead(pino_dir_dentro);
+  sensor_esq_fora = digitalRead(pino_esq_fora);
+  sensor_dir_fora = digitalRead(pino_dir_fora);
+
+  algumPreto = (sensor_esq_dentro == PRETO || sensor_dir_dentro == PRETO ||
+                sensor_dir_fora == PRETO || sensor_esq_fora == PRETO);
+
+  algumEsq = (sensor_esq_dentro == PRETO || sensor_esq_fora == PRETO);
+  algumDir = (sensor_dir_dentro == PRETO || sensor_dir_fora == PRETO);
+
+  todosPreto = (sensor_esq_dentro == PRETO && sensor_dir_dentro == PRETO &&
+                sensor_dir_fora == PRETO && sensor_esq_fora == PRETO);
+
+  todosEsq = (sensor_esq_dentro == PRETO && sensor_esq_fora == PRETO);
+  todosDir = (sensor_dir_dentro == PRETO && sensor_dir_fora == PRETO);
+
+  dentroPreto = (sensor_esq_dentro == PRETO || sensor_dir_dentro == PRETO);
+
+  foraPreto = ((sensor_esq_fora == PRETO || sensor_dir_fora == PRETO) && !dentroPreto);
+}
+
+float calc_ultra() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  unsigned long tempo = pulseIn(ECHO, HIGH, 20000); // timeout de 20ms
+  if (tempo == 0) return 999; // sem obstáculo
+  float dist = tempo * 0.034 / 2;
+  return dist;
+}
+
+void led(int r, int g, int b) {
+  analogWrite(R, r);
+  analogWrite(G, g);
+  analogWrite(B, b);
+}
+
+#define sensor_atual sensor_dir_fora
+
+void segue_faixa() {
+  atualizar_sensores();
+
+  if (algumDir) {
+    if (sensor_esq_fora == PRETO && sensor_esq_dentro == BRANCO) {
+      frente();
+      delay(150);
+    } else if (todosDir) {
+      direita();
+      delay(1000);
+      parar();
+    } else if (sensor_dir_dentro == PRETO && sensor_dir_fora == BRANCO) {
+      do {
+        atualizar_sensores();
+        direita();
+        delay(300);
+      } while (sensor_esq_dentro != PRETO);
+      parar();
+      delay(150);
+    } else if (foraPreto) {
+      frente();
+      delay(400);
+    }
+  } else if (algumEsq) {
+    if (sensor_esq_fora == PRETO && sensor_esq_dentro == BRANCO) {
+      frente();
+      delay(150);
+    } else if (todosEsq) {
+      esquerda();
+      delay(850);
+      parar();
+    } else if (sensor_esq_dentro == PRETO && sensor_esq_fora == BRANCO) {
+      do {
+        atualizar_sensores();
+        esquerda();
+        delay(200);
+      } while (sensor_dir_dentro != PRETO);
+      parar();
+      delay(150);
+    } else if (foraPreto) {
+      frente();
+      delay(400);
+    }
+  }
+}
